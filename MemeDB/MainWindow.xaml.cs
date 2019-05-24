510eb601f34e93d017f84d38afcffe457cc32700
Niko Lucinski <niko.lucinski@evosoft.com>
2019-05-24 16:32:38 +0200

    initial style setup

--- MemeDB/MainWindow.xaml.cs
+++ MemeDB/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace MemeDB
         {
             InitializeComponent();
         }
+
+
+
+
+        #region Window Bar
+
+        private void WindowBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = (this.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        #endregion
+
+        private void WindowBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed && this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                this.Top = 0;
+                
+                this.DragMove();
+            }
+        }
     }
 }
