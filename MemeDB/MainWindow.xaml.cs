e864a6b17da54f310af07716d6d4ad55db280a34
FlexXx1212 <33630274+FlexXx1212@users.noreply.github.com>
2019-05-25 00:59:43 +0200

    First break through with image display

--- MemeDB/MainWindow.xaml.cs
+++ MemeDB/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using MemeDB.Models;
+using NReco.VideoConverter;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace MemeDB
 {
@@ -20,16 +24,41 @@ namespace MemeDB
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Meme> Memes { get; set; }
+
         public MainWindow()
         {
+            Memes = new ObservableCollection<Meme>();
             InitializeComponent();
+
+            BitmapImage bi = new BitmapImage();
+
+            bi.BeginInit();
+            bi.UriSource = new Uri(@"D:\Videos\Recordings\GFX\photo.jpg");
+            bi.EndInit();
+
+            Meme m = new Meme("Photo", @"D:\Videos\Recordings\VFX\MCDAB.mp4", new string[] { "minecraft", "dab", "mc", "dabben", "lit", "fam" }, bi);
+            Memes.Add(m);
+
+
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                FFMpegConverter ffmpeg = new FFMpegConverter();
+                ffmpeg.GetVideoThumbnail(@"D:\Videos\Recordings\VFX\MCDAB.mp4", @"D:\Videos\Recordings\VFX\MCDAB_thumb.jpg");
+                BitmapImage img = new BitmapImage();
+                img.BeginInit();
+                img.UriSource = new Uri(@"D:\Videos\Recordings\VFX\MCDAB_thumb.jpg");
+                img.EndInit();
+
+                m = new Meme("MC Dab", @"D:\Videos\Recordings\VFX\MCDAB.mp4", new string[] { "minecraft", "dab", "mc", "dabben", "lit", "fam" }, img);
+                Memes.Add(m);
+            }
+
+
+            
         }
 
-
-
-
         #region Window Bar
-
         private void WindowBar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
@@ -50,7 +79,6 @@ namespace MemeDB
         {
             this.WindowState = WindowState.Minimized;
         }
-        #endregion
 
         private void WindowBar_MouseMove(object sender, MouseEventArgs e)
         {
@@ -58,9 +86,9 @@ namespace MemeDB
             {
                 this.WindowState = WindowState.Normal;
                 this.Top = 0;
-                
                 this.DragMove();
             }
         }
+        #endregion
     }
 }
---
+++ MemeDB/Models/Meme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MemeDB.Models
+{
+    public class Meme
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string[] Tags { get; set; }
+        public ImageSource Thumbnail { get; set; }
+
+        public Meme(string name, string path, string[] tags, ImageSource thumbnail)
+        {
+            Name = name;
+            Path = path;
+            Tags = tags;
+            Thumbnail = thumbnail;
+        }
+    }
+}
